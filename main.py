649b303f1e55773ba0b02454fbbcf852c0446103
kitesip <139232541+kitesip@users.noreply.github.com>
2023-08-02 13:44:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,3 @@
-import streamlit as st
-import numpy as np
-
-st.set_page_config(
-    page_title="CGPA Calculator",
-    page_icon="📊",
-    layout="centered",
-    initial_sidebar_state="auto",
-    menu_items={
-        "Get Help": "https://github.com/Siddhesh-Agarwal/CGPA-Calculator/discussions",
-        "Report a bug": "https://github.com/Siddhesh-Agarwal/CGPA-Calculator/issues/new",
-        "About": None,
-    },
-)
-
-
 grade_to_point = {
     "O": 10,
     "A+": 9,
